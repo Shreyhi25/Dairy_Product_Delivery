47b34de2667a5dc0734741c5e57fc16a52294efb
shreyhi25 <shreyhi25@gmail.com>
2025-05-02 18:56:49 +0530

    Updated files with recent changes

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-import os
-from backend.app import create_app
-
-# Set the environment
-env = os.getenv('FLASK_ENV', 'production')
-app = create_app(config_name=env)
+from backend.app import app
 
 if __name__ == "__main__":
     app.run() 
